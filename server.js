8e09b02f0dbffed1fa4691d5f9737e04d59f245a
gaurav.srivastava@safexpress.com+1-at-183454673550 <mauryagupteshwar@gmail.com>
2020-10-24 18:29:10 +0530

    recat js api on heroku

--- server.js
+++ server.js
@@ -24,6 +24,20 @@ app.use(cors({origin: ['*'],credentials: true}));
 
 app.use('/',cors(),rapi);
 
+//error handling
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 
 // app.get('/getapi',veryfyToken,(req,res)=>{
@@ -61,4 +75,17 @@ app.post('/postapi/login',(req,res)=>{
   //  res.json({message:"Data is posting"});
 });
 
-app.listen(2000,()=>console.log("Server is running On:2000"));
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || "3000";
+
+app.set("port", port);
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
